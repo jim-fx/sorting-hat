a31899b1b84f398ac2ebdcf95a45675ef1a13a4a
Jim Richter <jim-x@web.de>
2021-12-06 00:16:50 +0100

    fix ci

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,29 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterStatic from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 import glsl from 'vite-plugin-glsl';
 
+const { KIT_ADAPTER = 'static' } = process.env;
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
 
-  kit: {
-    adapter: adapter(),
+	kit: {
+		adapter: KIT_ADAPTER === 'static' ? adapterStatic() : adapterNode(),
 
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: 'body',
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: 'body',
 
-    vite: {
-      plugins: [glsl.default()],
-      ssr: {
-        noExternal: ['three'],
-      },
-    },
-  }
+		vite: {
+			plugins: [glsl.default()],
+			ssr: {
+				noExternal: ['three']
+			}
+		}
+	}
 };
 
 export default config;
