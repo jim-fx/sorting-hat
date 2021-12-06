8e1f8aabaaa44626d538b6ef1f4f5b8f686a4b10
Jim Richter <jim-x@web.de>
2021-12-06 01:53:23 +0100

    feat: compress some shit

--- src/lib/createHat.ts
+++ src/lib/createHat.ts
@@ -1,5 +1,10 @@
-import { MeshStandardMaterial, Scene, SkinnedMesh, TextureLoader, Vector2 } from "three";
-import { EquirectangularReflectionMapping } from "three";
+import {
+	EquirectangularReflectionMapping,
+	MeshStandardMaterial,
+	SkinnedMesh,
+	TextureLoader,
+	Vector2
+} from 'three';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import type { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
@@ -10,38 +15,32 @@ const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath('draco/');
 loader.setDRACOLoader(dracoLoader);
 
-export default async function(): Promise<GLTF> {
+export default async function (): Promise<GLTF> {
+	const tLoader = new TextureLoader();
+	const colMap = tLoader.load('textures/SortingHat_col.png');
+	const normalMap = tLoader.load('textures/SortingHat_normal.png');
+	const roughMap = tLoader.load('textures/SortingHat_rough.png');
+	const envMap = tLoader.load('textures/env_map.jpg');
+	envMap.mapping = EquirectangularReflectionMapping;
 
-  const tLoader = new TextureLoader();
-  const colMap = tLoader.load('SortingHat_col.png');
-  const normalMap = tLoader.load('SortingHat_normal.png');
-  const roughMap = tLoader.load('SortingHat_rough.png');
-  const envMap = tLoader.load('env_map.jpg');
-  envMap.mapping = EquirectangularReflectionMapping;
+	colMap.flipY = false;
+	normalMap.flipY = false;
+	roughMap.flipY = false;
 
-  colMap.flipY = false;
-  normalMap.flipY = false;
-  roughMap.flipY = false;
+	const mat = new MeshStandardMaterial({
+		map: colMap,
+		normalMap,
+		envMapIntensity: 1,
+		normalScale: new Vector2(1, 1),
+		roughnessMap: roughMap
+	});
 
-  const mat = new MeshStandardMaterial({
-    map: colMap,
-    normalMap,
-    envMapIntensity: 1,
-    normalScale: new Vector2(1, 1),
-    roughnessMap: roughMap
-  });
+	const gltf = await loader.loadAsync('sorting_hat.glb', (xhr) => {
+		console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+	});
 
+	const obj = gltf.scene.children[0].children[1] as SkinnedMesh;
+	obj.material = mat;
 
-  const gltf = await loader.loadAsync(
-    'sorting_hat.glb',
-    (xhr) => {
-      console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
-    }
-  );
-
-  const obj = gltf.scene.children[0].children[1] as SkinnedMesh;
-  obj.material = mat;
-
-  return gltf;
-
+	return gltf;
 }
