01d37c61e8d5471f41905ad0582f1101b88bb7b1
Jim Richter <jim-x@web.de>
2021-12-06 11:44:44 +0100

    fix(api): use cors proxy

--- src/lib/client-api.ts
+++ src/lib/client-api.ts
@@ -1,6 +1,6 @@
-const { VITE_API_URL = "" } = import.meta.env;
+const { VITE_API_URL = "" } = import.meta.env as unknown as { VITE_API_URL: string };
 
-async function send(url: string, method: string, body?: any) {
+async function send(_url: string, method: string, body?: any): Promise<Response> {
   const options = { method }
   if (body) {
     options["body"] = JSON.stringify(body);
@@ -8,16 +8,22 @@ async function send(url: string, method: string, body?: any) {
       "Content-Type": "application/json"
     }
   }
+
+  let url = VITE_API_URL + _url;
+  if (VITE_API_URL && VITE_API_URL.length) {
+    url = "https://thingproxy.freeboard.io/fetch/" + url
+  }
+
   return fetch(VITE_API_URL + "/" + url, options)
 }
 
-export function get(apiPath: string) {
+export function get(apiPath: string): Promise<Response> {
   return send(apiPath, "GET");
 }
 
 // To wake up the heroku instance
 get("api/houses");
 
-export function post(apiPath: string, body: unknown) {
+export function post(apiPath: string, body: unknown): Promise<Response> {
   return send(apiPath, "POST", body)
 }
