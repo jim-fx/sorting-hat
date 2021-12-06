e488b88e3adbd2dae7a052c1385a1de8d2a233a1
Jim Richter <jim-x@web.de>
2021-12-06 11:34:47 +0100

    fix: dont use process.env

--- src/lib/client-api.ts
+++ src/lib/client-api.ts
@@ -1,23 +1,23 @@
-const {VITE_API_URL = ""} = process.env;
+const { VITE_API_URL = "" } = import.meta.env;
 
-async function send(url:string, method:string, body?:any){
-  const options = {method}
-  if(body) {
+async function send(url: string, method: string, body?: any) {
+  const options = { method }
+  if (body) {
     options["body"] = JSON.stringify(body);
     options["headers"] = {
-      "Content-Type":"application/json"
+      "Content-Type": "application/json"
     }
   }
-  return fetch(VITE_API_URL+"/"+url, options)
+  return fetch(VITE_API_URL + "/" + url, options)
 }
 
-export function get(apiPath:string){
+export function get(apiPath: string) {
   return send(apiPath, "GET");
 }
 
 // To wake up the heroku instance
 get("api/houses");
 
-export function post(apiPath:string, body:unknown){
+export function post(apiPath: string, body: unknown) {
   return send(apiPath, "POST", body)
 }
