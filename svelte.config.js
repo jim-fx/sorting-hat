73efbaa52a521488ba62f09d05778f5df307edcb
Jim Richter <jim-x@web.de>
2021-12-06 01:17:53 +0100

    feat: some stuff

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
 import adapter from '@sveltejs/adapter-node';
+import adapterStatic from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 import glsl from 'vite-plugin-glsl';
+
+const { KIT_ADAPTER = 'node', BASE_PATH = '/' } = process.env;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,10 +12,14 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: KIT_ADAPTER === 'node' ? adapter() : adapterStatic(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: 'body',
+
+		paths: {
+			base: BASE_PATH
+		},
 
 		vite: {
 			plugins: [glsl.default()],
