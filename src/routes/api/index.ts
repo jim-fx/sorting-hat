f597f1cca91589b4b74e4758f1655e9e9a351cf5
Jim Richter <jim-x@web.de>
2021-12-06 01:13:15 +0100

    hide some api

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,20 +1,20 @@
-import { addPerson, getData } from "$lib/sheets"
+import { addPerson, getData } from '$lib/sheets';
+const { NODE_ENV = 'production' } = process.env;
 
 export async function get() {
-  return {
-    body: await getData()
-  }
+	if (NODE_ENV !== 'development') return;
+
+	return {
+		body: await getData()
+	};
 }
 
 export async function post({ body: { name, house, confidence } }) {
+	if (name && house && typeof confidence !== 'undefined') {
+		addPerson({ name, house, confidence });
+	}
 
-
-  if (name && house && typeof confidence !== "undefined") {
-    addPerson({ name, house, confidence })
-  }
-
-  return {
-    body: {}
-  }
-
+	return {
+		body: {}
+	};
 }
