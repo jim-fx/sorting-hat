b95c4cecc50be4de2c9882f1411019a907f79d1c
Jim Richter <jim-x@web.de>
2021-12-06 01:02:03 +0100

    fix: sopme

--- src/lib/sheets.ts
+++ src/lib/sheets.ts
@@ -1,99 +1,90 @@
-import { google } from "googleapis";
+import { google } from 'googleapis';
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: "credentials.json", //the key file
-  //url to spreadsheets API
-  scopes: "https://www.googleapis.com/auth/spreadsheets",
+	keyFile: 'google-credentials.json', //the key file
+	//url to spreadsheets API
+	scopes: 'https://www.googleapis.com/auth/spreadsheets'
 });
 
-const spreadsheetId = "1fbyfdAipqApzHY67jmDZaMNnQJPqpfef-ryrbfE-hFU";
+const spreadsheetId = '1fbyfdAipqApzHY67jmDZaMNnQJPqpfef-ryrbfE-hFU';
 
 const googleApi = (async () => {
+	const authClientObject = await auth.getClient();
 
-  const authClientObject = await auth.getClient();
+	const googleSheetsInstance = google.sheets({ version: 'v4', auth: authClientObject });
 
-  const googleSheetsInstance = google.sheets({ version: "v4", auth: authClientObject });
-
-  return googleSheetsInstance;
-
-})()
+	return googleSheetsInstance;
+})();
 
 async function getSheet() {
-  return await (await googleApi).spreadsheets.values.get({
-    auth, //auth object
-    spreadsheetId, // spreadsheet id
-    range: "GuestInput", //range of cells to read from.
-  })
+	return await (
+		await googleApi
+	).spreadsheets.values.get({
+		auth, //auth object
+		spreadsheetId, // spreadsheet id
+		range: 'GuestInput' //range of cells to read from.
+	});
 }
 
 function parseBoolean(o, key) {
-  if (key in o) { o[key] = o[key].length ? !!parseInt(o[key]) : null };
+	if (key in o) {
+		o[key] = o[key].length ? !!parseInt(o[key]) : null;
+	}
 }
 
 function parseNumber(o, key) {
-  if (key in o) { o[key] = o[key].length ? parseInt(o[key]) : null };
+	if (key in o) {
+		o[key] = o[key].length ? parseInt(o[key]) : null;
+	}
 }
 
+async function _getData(): Promise<{ Name: string; Alter: number }[]> {
+	const raw = await getSheet();
 
+	const _rows = raw.data.values;
 
-async function _getData(): Promise<{ Name: string, Alter: number }[]> {
-  const raw = await getSheet();
+	const [_, headers, ...rows] = _rows;
 
-  const _rows = raw.data.values;
+	function parseRow(row: string[]) {
+		const o = {};
 
+		row.forEach((v, i) => {
+			o[headers[i]] = v;
+		});
 
-  const [_, headers, ...rows] = _rows;
+		parseBoolean(o, 'Schlafen');
+		parseBoolean(o, 'Frauen');
+		parseBoolean(o, 'Single');
+		parseBoolean(o, 'Männer');
+		parseBoolean(o, 'Veggies');
 
-  function parseRow(row: string[]) {
-    const o = {};
+		parseNumber(o, 'Alter');
 
-    row.forEach((v, i) => { o[headers[i]] = v })
+		return o;
+	}
 
-    parseBoolean(o, "Schlafen");
-    parseBoolean(o, "Frauen");
-    parseBoolean(o, "Single");
-    parseBoolean(o, "Männer");
-    parseBoolean(o, "Veggies");
-
-    parseNumber(o, "Alter");
-
-    return o;
-  }
-
-  return rows.map(r => parseRow(r));
-
+	return rows.map((r) => parseRow(r));
 }
 
 let lastGetUpdate: number;
 let cacheData;
 export async function getData() {
-  if (!lastGetUpdate || Date.now() - 300000 > lastGetUpdate) {
-    cacheData = await _getData();
-    lastGetUpdate = Date.now();
-  }
+	if (!lastGetUpdate || Date.now() - 300000 > lastGetUpdate) {
+		cacheData = await _getData();
+		lastGetUpdate = Date.now();
+	}
 
-  return cacheData;
+	return cacheData;
 }
 
 export async function addPerson({ name, confidence, house }): Promise<unknown> {
-  return (await googleApi).spreadsheets.values.append({
-    auth, //auth object
-    spreadsheetId, // spreadsheet id
-    range: "GuestInput", //range of cells to read from.
-    valueInputOption: "RAW",
-    resource: {
-      values: [[
-        name,
-        confidence,
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        house
-      ]]
-    }
-  })
+	return (await googleApi).spreadsheets.values.append({
+		auth, //auth object
+		spreadsheetId, // spreadsheet id
+		range: 'GuestInput', //range of cells to read from.
+		valueInputOption: 'RAW',
+		resource: {
+			values: [[name, confidence, '', '', '', '', '', '', '', house]]
+		}
+	});
 }
