cb19fdded8c27bca7552a1c5c1830c8e8f800113
Jim Richter <jim-x@web.de>
2021-12-14 20:56:35 +0100

    securit commit

--- src/lib/quiz/data/main.ts
+++ src/lib/quiz/data/main.ts
@@ -1,19 +1,137 @@
-export default {
-	name: 'Harry Potter Quiz',
-	description: 'Midnight Hogwarts Quiz',
+const data: DataSet = {
+	description: 'Josies Hogwarts Quiz',
 	questions: [
 		{
-			description: 'Wie viele Häuser gibt es in Hogwarts?',
-			answers: ['1', '2', '3', '4'],
+			description: 'Welchem Haus gehört Nymphadora Tonks an? ',
+			answers: ['Gryffindor ', 'Ravenclaw ', 'Hufflepuff ', 'Slytherin '],
+			correct: 2
+		},
+		{
+			description: 'Wie lautet die Adresse der Dursleys',
+			answers: ['Magnolienalle 4', 'Ligusterweg 12', 'Magnolienalle 12', 'Ligusterweg 4 '],
 			correct: 3
 		},
 		{
-			description: 'How many hours are there in a day?',
-			answers: ['12', '24'],
+			description: 'Welche Position gibt es nicht beim Quidditch? ',
+			answers: ['Sucher*in', 'Stürmer*in ', 'Torhüter*in ', 'Jäger*in '],
 			correct: 1
 		},
 		{
-			description: 'Best House?'
+			description: 'Welcher Regisseur hat Harry Potter und der Gefangene von Askaban gedreht? ',
+			answers: ['David Yates ', 'Alfonso Cuarón ', 'Chris Columbus ', 'Mike Newell ']
+		},
+		{
+			description: 'Welche Augenfarbe hat Lily Potter? ',
+			answers: ['Blau ', 'Grau ', 'Grün ', 'Braun ']
+		},
+		{
+			description: 'Wer ist Peeves in den Harry Potter Büchern?',
+			answers: [
+				'Nerviger Hufflepuff-Junge',
+				'Hauself der Familie Tonks ',
+				'Poltergeist in Hogwarts ',
+				'Kröte von Neville '
+			]
+		},
+		{
+			description: 'Wie zerbricht der Zauberstab von Harry Potter?',
+			answers: [
+				'Durch Hermines Fluch ',
+				'Durch ein Duell zwischen Voldemort und Harry ',
+				'Durch die peitschende Weide ',
+				'Durch die Schlange Nagini '
+			]
+		},
+		{
+			description: 'Welche Form hat Ginny Weasleys Patronus‘? ',
+			answers: ['Otter', 'Hase ', 'Hund', 'Pferd ']
+		},
+		{
+			description:
+				'Mit wem geht Ron Weasly zu dem Weihnachtsball in Harry Potter und der Feuerkelch? ',
+			answers: ['Parvati Patil ', 'Lavender Brown ', 'Luna Lovegood ', 'Padma Patil ']
+		},
+		{
+			description: 'Was schenkte Sirius Harry an seinem ersten Geburtstag? ',
+			answers: ['Spielzeugzauberstab ', 'Spielzeugbesen ', 'Spielzeugschnatz ', 'Spielzeugeule']
+		},
+		{
+			description:
+				'Welches Verwandschaftsverhältnis besteht zwischen Bellatrix Lestrange und Nymphadora Tonks? ',
+			answers: ['Schwestern ', 'Cousinen ', 'Tante und Nichte ', 'Stiefmutter und Stieftochter']
+		},
+		{
+			description: 'Wie heißt der Bruder von Albus Dumbledore? ',
+			answers: [
+				'Wulfric Brian Dumbledore ',
+				'Aberforth Dumblebore',
+				'Adrian Dumbledore ',
+				'Percival Dumbledore '
+			]
+		},
+		{
+			description: 'Wer versucht Harry Potter einen Liebestrank zu verabreichen? ',
+			answers: ['Hannah Abbot ', 'Cho Chang ', 'Parvati Patil ', 'Romilda Vane ']
+		},
+		{
+			description: 'Welcher Gegenstand ist kein Horcrux von Lord Voldemort? ',
+			answers: [
+				'Kette von Rowina Ravenclaw',
+				'Tom Riddles Tagebuch ',
+				'Nagini ',
+				'Trinkbecher von Helga Hufflepuff '
+			]
+		},
+		{
+			description: 'In welches Wesen verwandelt sich der Irrwicht von Ron? ',
+			answers: ['Spinne ', 'Schlange ', 'Basilisk ', 'Ratte ']
+		},
+		{
+			description: 'Welchen Beruf führen Hermines Eltern in der Mugglewelt aus? ',
+			answers: [
+				'Lehrer*innen ',
+				'Zahnärzt*innen ',
+				'Angestellte in einer Bank ',
+				'Verwaltungsangestellte '
+			]
+		},
+		{
+			description: 'Welcher der folgenden Charaktere ist kein Animagus?',
+			answers: ['James Potter ', 'Minerva McGonagall', 'Rita Kimmkorn ', 'Remus Lupin ']
+		},
+		{
+			description: 'Welcher magische Gegenstand hilft Harry den Stein der Weisen zu erlangen? ',
+			answers: [
+				'Karte des Rumtreibers ',
+				'Sprechender Hut ',
+				'Godric Gryffindors Schwert ',
+				'Spiegel Nerhegeb'
+			]
+		},
+		{
+			description: 'Aus welchem Grund kann das Schwert von Godric Gryffindor Horcruxe zerstören? ',
+			answers: [
+				'Es wurde in Basilisken-Gift getränkt ',
+				'Es wurde von Kobolden angefertigt',
+				'Gryffindor selber hat es einem starken Zauber unterzogen ',
+				'Es wurde aus unzerstörbarem Silber hergestellt '
+			]
+		},
+		{
+			description: 'Wer tötet die Schlange Nagini? ',
+			answers: ['Harry Potter ', 'Neville Longbottom ', 'Ginny Weasley ', 'Albus Dumbledore ']
+		},
+		{
+			description:
+				'Welcher Satz muss gesagt werden, um die Karte des Rumtreibers unleslich zu machen? ',
+			answers: [
+				'Missetat begangen ',
+				'Ich schwöre feierlich, ich bin ein tunichtgut ',
+				'Unfug angestellt ',
+				'Dank den hochwohlgeborenen Herren Moony, Wurmschwanz, Tatze und Krone '
+			]
 		}
 	]
 };
+
+export default data;
