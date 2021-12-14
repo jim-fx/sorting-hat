7ad6097935fbb1a9876b15a39501c273259fd039
Jim Richter <jim-x@web.de>
2021-12-14 02:30:31 +0100

    fix: some shit

--- src/hooks.ts
+++ src/hooks.ts
@@ -19,8 +19,9 @@ export const handle: Handle = async ({ request, resolve }) => {
 	return {
 		...response,
 		headers: {
-			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Origin': 'silvester.jim-fx.com',
 			'Access-Control-Allow-Headers': 'Content-Type, Authentication',
+			'Access-Control-Request-Headers': 'Content-Type, Authentication',
 			'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
 			'Access-Control-Allow-Credentials': true,
 			...response.headers
--- src/routes/api/houses.ts
+++ src/routes/api/houses.ts
@@ -1,30 +1,29 @@
-import { getData } from "$lib/sheets";
+import { getData } from '$lib/sheets';
 
 export async function get() {
+	const d = await getData();
 
-  const d = await getData();
+	const houses = {
+		slytherin: 0,
+		gryffindor: 0,
+		hufflepuff: 0,
+		ravenclaw: 0
+	};
 
-  const houses = {
-    "slytherin": 0,
-    "gryffindor": 0,
-    "hufflepuff": 0,
-    "ravenclaw": 0,
-  }
+	d.forEach(({ Haus }) => {
+		if (Haus && Haus.toLowerCase() in houses) {
+			houses[Haus.toLowerCase()]++;
+		}
+	});
 
-  d.forEach(({ Haus }) => {
-    if (Haus && Haus.toLowerCase() in houses) {
-      houses[Haus.toLowerCase()]++;
-    }
-  })
-
-  return {
-    headers: {
-      "Content-Type": "text/plain",
-      "Access-Control-Allow-Origin": "*"
-    },
-    body: Object.entries(houses).map(([name, members]) => {
-      return { name, members }
-    }).sort((a, b) => a.members > b.members ? 1 : -1)[0].name
-  }
-
+	return {
+		headers: {
+			'Content-Type': 'text/plain'
+		},
+		body: Object.entries(houses)
+			.map(([name, members]) => {
+				return { name, members };
+			})
+			.sort((a, b) => (a.members > b.members ? 1 : -1))[0].name
+	};
 }
