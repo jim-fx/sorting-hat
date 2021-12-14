c9e934bbd0fc8833f021a6ef272283e2acd81955
Jim Richter <jim-x@web.de>
2021-12-14 01:35:13 +0100

    feat: load admin from env

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,19 +1,22 @@
 const {
 	VITE_MONGO_URL,
 	VITE_DB_NAME,
+	VITE_ADMIN_PASS,
 	VITE_JWT_SECRET = 'the-small-rabbit-jumped-in-the-murky-mist'
 } = import.meta.env;
 
 const MONGO_URL = VITE_MONGO_URL as string;
 const DB_NAME = VITE_DB_NAME as string;
 const JWT_SECRET = VITE_JWT_SECRET as string;
+const ADMIN_PASS = VITE_ADMIN_PASS as string;
 
 const env = {
+	ADMIN_PASS,
 	MONGO_URL,
 	DB_NAME,
 	JWT_SECRET
 };
 
-export { MONGO_URL, DB_NAME, JWT_SECRET };
+export { MONGO_URL, ADMIN_PASS, DB_NAME, JWT_SECRET };
 
 export default env;
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,14 @@
 import pkg from '@prisma/client';
 const { PrismaClient } = pkg;
 import { hash } from 'bcrypt';
+import { ADMIN_PASS } from '$lib/config';
 
 const prisma = new PrismaClient();
 
 (async () => {
 	let admin = await prisma.user.findFirst({ where: { name: 'admin' } });
-	if (!admin) {
-		const passwordHash = await hash('admin', 10);
+	if (!admin && ADMIN_PASS) {
+		const passwordHash = await hash(ADMIN_PASS, 10);
 		admin = await prisma.user.create({
 			data: { name: 'admin', created_at: new Date(), hash: passwordHash, role: 'ADMIN' }
 		});
