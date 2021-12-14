98c2fb38cc83aa4a6100ff0ebb5f9bbb37640556
Jim Richter <jim-x@web.de>
2021-12-14 22:54:28 +0100

    fucking cors, wtf

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -14,10 +14,11 @@ const prisma = new PrismaClient();
 
 	if (!admin && ADMIN_PASS) {
 		const passwordHash = await hash(ADMIN_PASS, 10);
-		console.log('CREATE');
+		console.log('CREATE', passwordHash);
 		admin = await prisma.user.create({
 			data: { name: 'admin', created_at: new Date(), hash: passwordHash, role: 'ADMIN' }
 		});
+		console.log('ADMIN', admin);
 	}
 })();
 
