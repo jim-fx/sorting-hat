fdbd773d4bbdc613ca06b30335b944d77bab279d
jim-fx <jim-x@web.de>
2021-12-20 15:19:35 +0000

    Deploying to gh-pages from @ jim-fx/sorting-hat@1f9d3f898451a6998a61510481d7fc7f9a2c3fd7 🚀

---
+++ _app/pages/admin/index.svelte-41d00a01.js
@@ -0,0 +1,1 @@
+import{S as i,i as c,s as l,e as d,t as p,c as m,a as f,g as u,d as r,f as _,G as h,H as n}from"../../chunks/vendor-4d1406ba.js";function x(o){let t,s;return{c(){t=d("p"),s=p("Start")},l(a){t=m(a,"P",{});var e=f(t);s=u(e,"Start"),e.forEach(r)},m(a,e){_(a,t,e),h(t,s)},p:n,i:n,o:n,d(a){a&&r(t)}}}class v extends i{constructor(t){super();c(this,t,null,x,l,{})}}export{v as default};
